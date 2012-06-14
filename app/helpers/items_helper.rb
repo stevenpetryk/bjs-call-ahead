cc1b58fe612fd8948083a42219909fee3ec08744
Steven Petryk <me@stevenpetryk.com>
2012-06-14 12:26:23 -0400

    Finished ajax refactoring

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,17 @@
 module ItemsHelper
+	def favorites_link_for(item)
+		if item.favorite
+			asset_path 'favorited.png' 
+		else
+			asset_path 'notfavorited.png' 
+		end
+	end
+
+	def process_disclaimer(string)
+		# Matches [*=May contain raw or uncooked meats]
+		disclaimerPattern = /\ \[\*\=(.+)\]/
+
+		# RegExp replace
+		string.sub(disclaimerPattern, ' <span class="tooltip" data-tooltip="$1">*</span>')
+	end
 end
